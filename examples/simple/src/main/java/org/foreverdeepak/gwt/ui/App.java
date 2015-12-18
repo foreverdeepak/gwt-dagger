a05318c072eaf4a7b166a652cdd712e2245bd4a0
Deepak Chauhan <foreverdeepak@gmail.com>
2015-12-18 19:35:09 +0530

    Initial commit

---
+++ examples/simple/src/main/java/org/foreverdeepak/gwt/ui/App.java
@@ -0,0 +1,16 @@
+package org.foreverdeepak.gwt.ui;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Created by deepakc on 18/12/15.
+ */
+public class App implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        Window.alert("Testing");
+    }
+}
+
